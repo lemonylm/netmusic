faccebdbf2c3059d2948419392487235e59a2302
wangdi777 <2721690395@qq.com>
2021-07-02 09:36:52 +0800

    update

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,15 +1,39 @@
 <template>
   <div id="nav">
-    <router-link :to="{ path: '/discover' }">Discover</router-link> |
-    <router-link :to="{ path: '/mymusic' }">MyMusic</router-link>
+    <div class="nav_header">
+      <div class="nav_header_main">
+        <img class="logo" src="" alt="">
+        <router-link :class="{active: path === 'discover'}" :to="{ path: '/discover' }">Discover</router-link> |
+        <router-link :class="{active: path === 'mymusic'}" :to="{ path: '/mymusic' }">MyMusic</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Header",
+  props: ['path']
 };
 </script>
 
 <style lang="less" scoped>
+.active {
+  color: red;
+}
+#nav {
+  .nav_header {
+    height: 70px;
+    background: #242424;
+    .nav_header_main {
+      width: 1100px;
+      height: 100%;
+      margin: 0 auto;
+      .logo {
+        width: 177px;
+        height: 59px;
+      }
+    }
+  }
+}
 </style>
